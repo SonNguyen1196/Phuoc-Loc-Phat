9b88c8b960410dc35e0b396623721a766be6de32
Ngoc Son Dev <son@firestak.com>
2019-06-30 13:17:15 +0700

    Access Auto Post

---
+++ wp-content/plugins/accesspress-facebook-auto-post/inc/header.php
@@ -0,0 +1,25 @@
+<?php defined('ABSPATH') or die('No script kiddies please!');?>
+<div class="asap-header">
+    <h3>AccessPress Social Auto Post Lite</h3>
+    <span class="asap-version">V <?php echo AFAP_VERSION; ?></span>
+    <div class="asap-social-wrap">
+        <p><?php _e('Follow us for new updates', 'accesspress-facebook-auto-post') ?></p>
+        <div class="ap-social-bttns">
+
+            <iframe src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fpages%2FAccessPress-Themes%2F1396595907277967&amp;width&amp;layout=button&amp;action=like&amp;show_faces=false&amp;share=false&amp;height=35&amp;appId=1411139805828592" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:20px; width:50px " allowTransparency="true"></iframe>
+            &nbsp;&nbsp;
+            <a href="https://twitter.com/apthemes" class="twitter-follow-button" data-show-count="false" data-lang="en">Follow @apthemes</a>
+            <script>!function (d, s, id) {
+                    var js, fjs = d.getElementsByTagName(s)[0];
+                    if (!d.getElementById(id)) {
+                        js = d.createElement(s);
+                        js.id = id;
+                        js.src = "//platform.twitter.com/widgets.js";
+                        fjs.parentNode.insertBefore(js, fjs);
+                    }
+                }(document, "script", "twitter-wjs");</script>
+
+        </div>
+    </div>
+</div>
+ <p class="asap-authorize-note apfap-graph-api-options"><?php _e('Important Note: This plugin is replacement of our plugin "AccessPress Facebook Auto Post" This is not an official APP of Facebook but is a Free WordPress plugin which only helps you to auto post any post ,pages or custom post type content to your facebook account in your profile or fan pages.', 'accesspress-facebook-auto-post'); ?></p>
