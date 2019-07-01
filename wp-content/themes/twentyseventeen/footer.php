01cbe2f54088dd80c3c8da1b74702eda9addec1e
Ngoc Son Dev <son@firestak.com>
2019-07-01 17:35:45 +0700

    Live Chat Update

--- wp-content/themes/twentyseventeen/footer.php
+++ wp-content/themes/twentyseventeen/footer.php
@@ -95,7 +95,7 @@ var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/5d061af036eab9721117af75/default';
+s1.src='https://embed.tawk.to/5d19e1bc7a48df6da2425be2/default';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
